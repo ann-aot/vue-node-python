c1055fef1f4408ad8c8bb7101e44946aeed81562
divyav-aot <divyav-aot@github>
2025-08-29 17:58:37 -0400

    change to the API_BASE

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,1 +1,7 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8300/api/v1';
+// export const API_BASE = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8300/api/v1';
+
+export const API_BASE =
+  import.meta.env.VITE_API_BASE_URL ||
+  (window.location.hostname.includes('gitpod.io')
+    ? window.location.origin.replace('4000', '8300') + '/api/v1'
+    : 'http://localhost:8300/api/v1');
